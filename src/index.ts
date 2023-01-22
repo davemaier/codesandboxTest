82fb0fe72611f9c68b1c6cc6ab91e084d745457b
davemaier <david.maier@atempo.at>
2023-01-22 21:22:29 +0000

    this is just a test for the previews

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 async function listAllUsers() {
-  const prisma = new PrismaClient()
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+  const prisma = new PrismaClient();
+  const allUsers = await prisma.user.findMany();
+
+  console.log(allUsers);
 }
 
-listAllUsers()
+listAllUsers();
